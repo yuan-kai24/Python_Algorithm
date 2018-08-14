6446b9113522ab88f67441ce160db6443615e2af
yuankai <yuan.kai.24@foxmail.com>
2018-08-14 23:17:15 +0800

    ok

--- ChessAssessment.py
+++ ChessAssessment.py
@@ -1,3 +1,94 @@
+
+
+# 满分版本。。。
+A = []
+
+
+def rck(i, val):  # 横
+    return val == A[i][0] == A[i][1] == A[i][2]
+
+
+def cck(j, val):  # 竖
+    return val == A[0][j] == A[1][j] == A[2][j]
+
+
+def dck(val):  # 斜
+    return (A[0][0] == val == A[1][1] == A[2][2]) or (A[0][2] == val == A[1][1] == A[2][0])
+
+
+def zeroCpt():
+    res = 0
+    for item in range(0, 3):
+        for ite in range(0, 3):
+            if A[item][ite] == 0:
+                res = res + 1
+    if res == 9:
+       return 0
+    else:
+       return res
+
+
+def win(player):
+    r = rck(0, player) or rck(1, player) or rck(2, player)
+    c = cck(0, player) or cck(1, player) or cck(2, player)
+    d = dck(player)
+    w = r or c or d
+    if not w:
+        return 0
+    w = zeroCpt() + 1
+    return player == 1 and w or -w
+
+
+def dfs(player):
+    if zeroCpt() == 0:
+        return 0
+    Min = 10
+    Max = -10
+    global A
+    for item in range(0, 3):
+        for ite in range(0, 3):
+            if A[item][ite] == 0:
+                A[item][ite] = player
+                w = win(player)
+                if w != 0:
+                    A[item][ite] = 0
+                    if w > 0:
+                        return max(Max, w)
+                    else:
+                        return min(Min, w)
+                if player == 1:
+                    Max = max(Max, dfs(2))
+                else:
+                    Min = min(Min, dfs(1))
+                A[item][ite] = 0
+    if player == 1:
+        return Max
+    else:
+        return Min
+
+
+def main1():
+    T = int(input())
+    global A
+    while T > 0:
+        for item in range(0, 3):
+            A.append(input().split())
+            for ite in range(0, 3):
+                A[item][ite] = int(A[item][ite])
+        x = win(1)
+        y = win(2)
+        if x != 0:
+            print(x)
+        elif y != 0:
+            print(y)
+        else:
+            print(dfs(1))
+        A = []
+        T = T-1
+
+
+
+# 90分版本。。。。无语
 usercount = int(input())  # 用户输入
 chess = []  # 棋局存储
 
@@ -18,17 +109,18 @@ def findspa(val, v):  # 查询空格
         for item2 in range(0, 3):
             if val[item][item2] == "0":  # 记录空格
                 count = count + 1
-    if count == 0:  # 棋局下完，平局情况
-        return count
     count = count + 1
-    return v == '1' and count or -count
+    if v == '1':
+        return count
+    else:
+        return -count
 
 
 def win(val):  # 判断胜负
     fenshu = -10
     for item in range(0, 3):  # 横向
-            if val[item][0] == val[item][1] == val[item][2] != '0':
-                fenshu = findspa(val, val[item][0])
+        if val[item][0] == val[item][1] == val[item][2] != '0':
+            fenshu = findspa(val, val[item][0])
 
     for item in range(0, 3):  # 纵向
         if val[0][item] == val[1][item] == val[2][item] != '0':
@@ -40,32 +132,40 @@ def win(val):  # 判断胜负
         fenshu = findspa(val, val[0][2])
     if ['0', '0', '0'] == val[0] == val[1] == val[2]:  # emmmm
         return 0
+    if not '0' in val[0] and not '0' in val[1] and not '0' in val[2]:
+        return 0
     return fenshu
 
 
 def dfs(val, count):
-    if findspa(val, '1') == 0:
+    if win(val) == 0:
         return 0
-    Max : int = -10  # 策略1号最优
-    Min : int = 10  # 策略2号最优
-    for item in range(1, 3):
-        for ite in range(1, 3):
+    Max = -10  # 策略1号最优
+    Min = 10  # 策略2号最优
+    for item in range(0, 3):
+        for ite in range(0, 3):
             if val[item][ite] == '0':  # 判断是否可以落子
                 val[item][ite] = str(count)
                 w = win(val)
                 if w != -10:  # 判断胜负
                     val[item][ite] = '0'
-                    return w > 0 and max(w, Max) or min(w, Min)  # 选极值
+                    if w > 0:
+                        return max(w, Max)
+                    else:
+                        return min(w, Min)
 
                 if count == 1:  # 判断下棋人
                     Max = max(Max, dfs(val, 2))
                 else:
                     Min = min(Min, dfs(val, 1))
                 val[item][ite] = '0'  # 回溯
-    return count == 1 and Max or Min
+    if count == 1:
+        return Max
+    else:
+        return Min
 
 
-def main():
+def main2():
     init()
     for item in range(0, usercount):  # 棋局分配
         w = win(chess[item])
@@ -73,4 +173,6 @@ def main():
             print(w)
         else:
             print(dfs(chess[item], 1))
-main()
+
+
+main2()
