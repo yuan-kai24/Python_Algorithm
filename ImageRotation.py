0f435d474e6ddd99820417b53cc6059763fa39d1
yuan-kai24 <yuan.kai.24@foxmail.com>
2018-09-12 19:55:47 +0800

    图像旋转

--- ImageRotation.py
+++ ImageRotation.py
@@ -1,13 +1,13 @@
 user = input()
-user = user.split(" ")
+user = user.split(" ")# 输入格式拆分
 photo = []
 for item in range(0, int(user[0])):
     userin = input()
-    photo.append(userin.split(" "))
+    photo.append(userin.split(" "))# 以行为单位存储数据
 
 countx = int(user[1])-1
 while countx >= 0:
-    for item in range(0, int(user[0])):
+    for item in range(0, int(user[0])):# 从行最后反向从列遍历
         print(photo[item][countx], end=" ")
-    print()
-    countx = countx - 1
+    print()# 换行
+    countx = countx - 1 # 递减
