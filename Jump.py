0d4051abf7e06a49c95732d3d8cf6c43f4083b5a
yuan-kai24 <yuan.kai.24@foxmail.com>
2018-09-12 19:31:24 +0800

    跳一跳

--- Jump.py
+++ Jump.py
@@ -9,17 +9,17 @@ def jump():
     tag = 1
 
     for num in scan:
-        if num == "0":
+        if num == "0":# 等于零时退出
             break
-        if num == "1":
+        if num == "1":# 跳上去，没跳到中心情况
             tag = 1
             jump = jump + tag
-        if num == "2":
-            if tag == 1:
+        if num == "2":# 跳到中心情况
+            if tag == 1:# 第一次跳到中心
                 tag = 2
-            else:
+            else:# 第二次后跳到中心
                 tag = tag + 2
-            jump = jump + tag
+            jump = jump + tag# 加分
 
     print(jump)
 
