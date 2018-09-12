85e202ba03c89b843ad516f0bacc290a0c1e4a8e
yuan-kai24 <yuan.kai.24@foxmail.com>
2018-09-12 20:01:42 +0800

    最小差值

--- MinimumDifference.py
+++ MinimumDifference.py
@@ -1,12 +1,12 @@
 count = input()
 userInfo = input()
-userInfo = userInfo.split()
+userInfo = userInfo.split()# 拆分
 min = 10000
 rel = 0
-for i in range(0, userInfo.__len__()):
-    for item in range(i+1, userInfo.__len__()):
+for i in range(0, userInfo.__len__()):# 遍历每个数据
+    for item in range(i+1, userInfo.__len__()):# 用每个数据依次与其他数据遍历
         rel = int(userInfo[i]) - int(userInfo[item])
-        rel = abs(rel)
+        rel = abs(rel)# 绝对值
         if rel < min:
             min = rel
 print(min)
