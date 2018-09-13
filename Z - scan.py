77c03059e87361ee9d22cab8d5180e15c1d247d5
yuan-kai24 <yuan.kai.24@foxmail.com>
2018-09-13 23:56:28 +0800

    z

--- Z - scan.py
+++ Z - scan.py
@@ -2,42 +2,14 @@ count = int(input())
 userInfo = []
 for item in range(0, count):
     userInfo.append(input().split())# 存储数据
-# for item in range(0, count*2-1):# 斜面执行2n-1次
-#     while True:
-#         coor[0] = coor[0] + coorC[0]  # 坐标增减
-#         coor[1] = coor[1] + coorC[1]  # 同上
-#         print(userInfo[coor[0]][coor[1]], end=" ")# 输出
-#         if coor == 0 or coor[1] == 0:# 推出条件
-#             if coor[0] == 0:
-#                 coor[1] = coor[1] + 1
-#             else:
-#                 coor[0] = coor[0] + 1
-#             print(userInfo[coor[0]][coor[1]], end=" ")  # 输出
-#             break
-#         if coor[0] == (count-1) or coor[1] == (count-1):
-#             if coor[0] == (count-1):
-#                 coor[1] = coor[1] + 1
-#             else:
-#                 coor[0] = coor[0] + 1
-#             print(userInfo[coor[0]][coor[1]], end=" ")  # 输出
-#             break
-#     if coor[0] == 0:# 增量改变方式
-#         coorC[0] = -1
-#         coorC[1] = 1
-#     if coor[1] == 0:
-#         coorC[0] = 1
-#         coorC[1] = -1
-coor = [0, 0]
-coorx = [0, 0]
-num = 0
-numx = count - 1
+coor = [0, 0]# 坐标初始化
+coorx = [0, 0]# 坐标移动
+num = 0# 坐标头
+numx = count - 1 #过半时操作
 fx = True
 for item in range(0, count*2-1):# 斜面执行2n-1次
-    if item < count:
+    if fx: #判断是否过半
         num = item
-    else:
-        num = numx - (item - numx) + 1
-    if fx:
         if (num%2) == 0:
             coor[0] = num
             coor[1] = 0
@@ -45,6 +17,7 @@ for item in range(0, count*2-1):# 斜面执行2n-1次
             coor[0] = 0
             coor[1] = num
     else:
+        num = numx - (item - numx) + 1
         if (num%2) == 0:
             coor[0] = num
             coor[1] = count-1
@@ -53,6 +26,5 @@ for item in range(0, count*2-1):# 斜面执行2n-1次
             coor[1] = num
     if (count-1) == item:
         fx = False
-    if item == (2*count-2):
-        coor[0] = coor[1] = count-1
-    print(userInfo[coor[0]][coor[1]], "-----------------", num ,coor)
+    for ite in range(0, num):
+        pass
