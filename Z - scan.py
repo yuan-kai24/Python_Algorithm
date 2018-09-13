55de03c12376d04b311183d4d3f62d367fc7702a
yuan-kai24 <yuan.kai.24@foxmail.com>
2018-09-14 00:16:17 +0800

    z

--- Z - scan.py
+++ Z - scan.py
@@ -13,18 +13,28 @@ for item in range(0, count*2-1):# 斜面执行2n-1次
         if (num%2) == 0:
             coor[0] = num
             coor[1] = 0
+            coorx[0] = -1
+            coorx[1] = 1
         else:
             coor[0] = 0
             coor[1] = num
+            coorx[0] = 1
+            coorx[1] = -1
     else:
         num = numx - (item - numx) + 1
         if (num%2) == 0:
             coor[0] = num
             coor[1] = count-1
+            coorx[0] = -1
+            coorx[1] = 1
         else:
             coor[0] = count-1
             coor[1] = num
+            coorx[0] = 1
+            coorx[1] = -1
     if (count-1) == item:
         fx = False
-    for ite in range(0, num):
-        pass
+    for ite in range(0, num+1):
+        print(userInfo[coor[0]][coor[1]], coor, coorx,num)
+        coor[0] = coor[0] + coorx[0]
+        coor[1] = coor[1] + coorx[1]
